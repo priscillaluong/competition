4aa6b7c1435b59c33cc829b7e318cb59d0dbf7c0
wynchung <wynchung@hotmail.co.uk>
2017-07-06 21:00:33 +0100

    Added navbar scrolling jQeury code
    Still glitchy so looking to improve

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+$( document ).ready(function() {
+  $("#button1").on("click", function() {
+    alert("clicked!")
+    });
+});
 
-  $( document ).ready(function() {
-  	$("#button1").on("click", function() {
-  alert("clicked!")
+//Sticky navbar
+$(document).ready(function() {
+  //change the integers below to match the height of your upper dive, which I called
+  //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+  //to figure out what the scroll position is when exactly you want to fix the nav
+  //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+  //you know the position.
+  $(window).scroll(function () {
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 100) {
+      $('.navbar').addClass('navbar-fixed-top');
+    }
+    if ($(window).scrollTop() < 101) {
+      $('.navbar').removeClass('navbar-fixed-top');
+    }
   });
-
-  	//events to be added
-  });
+});

9dd7777cff2a6f56261f5bad7cbbc3b28b2b594e
priscillaluong <p-luong@live.co.uk>
2017-07-15 23:30:29 +0100

    removed maps js code

--- script.js
+++ script.js
@@ -9,32 +9,6 @@ $(document).ready(function() {
   $('li.active').removeClass('active');
   $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
 });
-
-$(document).ready(function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var myCenter = new google.maps.LatLng(51.522670,-0.085638);
-  var mapOptions = {center: myCenter, zoom: 16};
-  var map = new google.maps.Map(mapCanvas,mapOptions);
-  var infowindow = new google.maps.InfoWindow({});
-  var icon = {
-  url: "images/cosmea-Flower.png",
-  scaledSize: new google.maps.Size(50, 50),
-  origin: new google.maps.Point(0, 0),
-  anchor: new google.maps.Point(0, 0),
-  };
-  var marker = new google.maps.Marker({
-  position: new google.maps.LatLng(51.522670,-0.085638),
-  icon: icon,
-  optimized: false,
-  animation:google.maps.Animation.BOUNCE,
-  });
-  marker.setMap(map);
-  google.maps.event.addListener(marker,'click',function() {
-  var infowindow = new google.maps.InfoWindow({
-  content:"Baxters. Make someone happy."
-  });
-  infowindow.open(map,marker);
-  });
   
 /*
 //Sticky navbar
